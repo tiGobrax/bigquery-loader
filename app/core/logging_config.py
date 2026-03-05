d7b4412e2240ad64ce9f5ed4fd2a6d670a6ff9b4
Carlos Dutra <henrique.r.dutra@gmail.com>
2026-03-05 12:01:53 -0300

    delete all

--- app/core/logging_config.py
+++
@@ -1,5 +0,0 @@
-from loguru import logger
-import sys
-
-logger.remove()
-logger.add(sys.stdout, level="INFO", format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level}</level> | {message}")
